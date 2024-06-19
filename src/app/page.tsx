2dec01c5cfcb77c51b0ed3475b01b2b7eaab2750
Abhishek <chaudharyabhishekmp@gmail.com>
2024-06-19 16:10:02 +0530

    initial commit

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.0">
-      <h1 className="text-2xl text-center">Hello there</h1>
-    </main>
-  );
-}
